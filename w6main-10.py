80bcd296690e08fb5fbe9c7bf19b59ea4b212ce5
amiva <wntjrdbs@naver.com>
2016-04-06 14:46:13 +0900

    6주차 수업시간 문제 제어-10

---
+++ w6main-10.py
@@ -0,0 +1,32 @@
+﻿def main():
+	sum=0
+	for i in range(1,1001):
+		if(i%3==0 or i%5==0):
+			sum=sum+i
+	print sum
+main()
+import turtle
+wn=turtle.Screen()
+wn.exitonclick()
+
+
+
+"""
+diagram
+@startuml
+
+title 3과5의배수 더하기 
+
+
+start
+
+repeat
+if(i is divided by 3 or 5)then(plus)
+:sum=0;
+:sum=sum+i;
+else
+:throw away;
+endif
+
+repeat while(1<i<=1000)
+"""
