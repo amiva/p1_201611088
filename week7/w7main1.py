9aacdc3742a762f2aeb70875f958de15d9eac96f
amiva <wntjrdbs@naver.com>
2016-04-18 14:21:51 +0900

    7주차 list와 dict를 활용한 도형그리기

--- week7/w7main1.py
+++ week7/w7main1.py
@@ -2,12 +2,18 @@
 wn=turtle.Screen()
 t1=turtle.Turtle()
 tracks=list()
-def lab7():
+def drawSquareAtSave(size,pos):
+	t1.penup()
+	t1.goto(pos)
+	t1.pendown()	
 	for i in range(0,4):
 		tracks.append(t1.pos())
 		t1.fd(50)
 		t1.right(90)
 		print tracks
+def lab7():
+	drawSquareAtSave(100,(0,0))
+
 def main():
 	lab7()
 if __name__=="__main__":
