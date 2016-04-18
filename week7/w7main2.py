0318bba9585aa1b60eb8d3cf927a84261e4bddb4
amiva <wntjrdbs@naver.com>
2016-04-18 14:25:27 +0900

    7주차 list와 dict를 이용한 도형그리기

--- week7/w7main2.py
+++ week7/w7main2.py
@@ -1,16 +1,21 @@
 ﻿import turtle
 wn=turtle.Screen()
 t1=turtle.Turtle()
-c=dict()
-c=({0:(0,0),1:(100,0),2:(100,100),3:(0,100),4:(0,0)})
+tracks=list()
+def drawSquareAtSave(size,pos):
+	t1.penup()
+	t1.goto(pos)
+	t1.pendown()	
+	for i in range(0,4):
+		tracks.append(t1.pos())
+		t1.fd(50)
+		t1.right(90)
+		print tracks
 def lab7():
-	for i in range(1,5):
-		t1.goto(c[i])
-	for j in range(1,5):
-		print c[j]
+	drawSquareAtSave(100,(0,0))
+
 def main():
 	lab7()
 if __name__=="__main__":
-	main()
-
-wn.exitonclick()
+	main()	
+wn.exitonclick()	
---
+++ week7/w7main3.py
@@ -0,0 +1,16 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+c=dict()
+c=({0:(0,0),1:(100,0),2:(100,100),3:(0,100),4:(0,0)})
+def lab7():
+	for i in range(1,5):
+		t1.goto(c[i])
+	for j in range(1,5):
+		print c[j]
+def main():
+	lab7()
+if __name__=="__main__":
+	main()
+
+wn.exitonclick()
