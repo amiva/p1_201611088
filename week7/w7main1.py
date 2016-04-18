e5f9f317c1af3c27cf8e0b9d9cb39d8c970b9cf7
amiva <wntjrdbs@naver.com>
2016-04-18 14:03:08 +0900

    7주차 dict와 list를 이용한 도형그리기

---
+++ week7/w7main1.py
@@ -0,0 +1,10 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+tracks=list()
+for i in range(0,4):
+	tracks.append(t1.pos())
+	t1.fd(50)
+	t1.right(90)
+print tracks	
+wn.exitonclick()	
---
+++ week7/w7main2.py
@@ -0,0 +1,10 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+c=dict()
+c=({0:(0,0),1:(100,0),2:(100,100),3:(0,100),4:(0,0)})
+for i in range(1,5):
+	t1.goto(c[i])
+for j in range(1,5):
+	print c[j]
+wn.exitonclick()
