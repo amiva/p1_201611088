a8e444ac91853e34abe72821db382c65496a7445
amiva <wntjrdbs@naver.com>
2016-05-03 01:55:30 +0900

    8주차 과제

---
+++ week8/w8main3-가전제품찾기1.py
@@ -0,0 +1,21 @@
+﻿myhome=list()
+friendhome=list()
+myhome=['tv','phone','camera','fridger','mixer','audio','cdplayer','light','computer','notebook','recorder']
+friendhome=['coffeemachine','radio','camera','runningmachine','ramp','computer','notebook','recorder']
+print myhome
+print friendhome
+k=friendhome
+k1=set(myhome)&set(friendhome)
+k2=myhome
+x1=list()
+x2=list()
+print k1
+for c in friendhome:
+    if c in k and c not in k1:
+        x1.append(c)
+print "the thing that is not in your home and that is in your friendhome is" , x1 
+for t in myhome:
+    if t in k2 and t not in k1:
+	x2.append(t)
+print "the thing that is not in your friendhome and that is in your home is" , x2
+input()
