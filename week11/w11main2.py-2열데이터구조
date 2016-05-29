550962125ed35046f0aad7bc845688db7f2de98b
amiva <wntjrdbs@naver.com>
2016-05-29 11:09:35 +0900

    201611088 윤주석

---
+++ week11/w11main2.py-2열데이터구조
@@ -0,0 +1,11 @@
+﻿data=[1,2,3,4,5,6,7,8,9,10]
+fout=open('outputNumber1.txt','w')
+for i in data:
+    if i%2==0:
+        toPrint="{0}\t\n".format(i)
+    else:
+        toPrint="{0}\t".format(i)    
+    print toPrint,
+    fout.write(toPrint)
+fout.close()
+input("")
---
+++ week12/w12main1.py
@@ -0,0 +1,19 @@
+﻿import time
+timeEdit=time.strftime('%Y-%m-%d %H:%M:%S')
+editor='yjs'
+edtied="[{0} edited {1}]".format(editor,timeEdit)
+fin=open('output.txt','r')
+fout=open('outputUpper.txt','w')
+type(fin)
+for line in fin:
+    words=line.split()
+    for word in words:
+        if word=='line':
+            word=word.upper()
+            fout.write(word)
+        elif not word=='line':
+            fout.write(word)
+    print "\n"        
+    print line
+fin.close()    
+fout.close()    
