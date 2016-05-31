093bd25ec5bd8020011d50a137dd19bc0bb2bf21
amiva <wntjrdbs@naver.com>
2016-05-31 14:22:26 +0900

    201611088 윤주석

---
+++ week13/w13main2.py
@@ -0,0 +1,25 @@
+﻿import time
+def homework():
+    filetwo=open('output.txt', 'w')
+    line1='first line\n'
+    filetwo.write(line1)
+    line2='second line\n'
+    filetwo.write(line2)
+    line3='third line'
+    filetwo.write(line3)
+    filetwo.close()
+    msg='[Yjs edited {0}]'.format(time.strftime('%Y.%m.%d , %H:%M:%S'))
+    fin=open('output.txt', 'r')
+    fout=open('outputUpper.txt','w')
+    for line in fin:
+        words=line.split()
+        fout.write(msg)
+        fout.write('\t')
+        for word in words:
+            if word == 'line':
+                fout.write('\t')    
+                word=word.upper()
+            fout.write(word)
+        fout.write('\n')
+    fin.close()
+    fout.close()
---
+++ week13/w13main3.py
@@ -0,0 +1,22 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def drawSquarefromFile():
+    frec=open('polygon1.txt')
+    mycoords=[]
+    for line in frec:
+        line1=line.split(',')
+        mycoords.append([(line1[0],line1[1]),(line1[2],line1[3].strip())])
+        for coord in mycoords:
+            x1=int(coord[0][0])
+            x2=int(coord[1][0])
+            y1=int(coord[0][1])
+            y2=int(coord[1][1])
+        coord_li=((x1,y1),(x2,y1),(x2,y2),(x1,y2),(x1,y1))
+        t1.penup()
+        t1.setpos(coord_li[0])
+        t1.pendown()
+        for c in coord_li:
+            t1.goto(c)   
+    frec.close()
